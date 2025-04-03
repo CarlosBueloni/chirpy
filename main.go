d7424d272cf3ef76bbac82e90af6c30f5ed4aca0
CarlosBueloni <carlosbueloni@gmail.com>
2025-04-03 16:52:42 -0300

    add metrics

--- main.go
+++ main.go
@@ -3,19 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
-	mux := http.NewServeMux()
 	port := "8080"
 	filepathRoot := "."
+
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET  /api/healthz", handlerReadiness)
+	mux.HandleFunc("GET  /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handleReset)
 
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-	mux.HandleFunc("/healthz", handlerReadiness)
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
